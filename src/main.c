b5997769e838c493abb15af0250a6f26dd74a147
raphael-ph <raphasilvaesilva15@gmail.com>
2020-08-18 01:06:15 -0300

    Coloquei mais comentarios e deixei o codigo mais limpo

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
  *
  * Raphael Carvalho - RA: 205125
  * Laboratorio - 06
+ * A maior parte do codigo utilizado foi disponibilizado pelo professor.
+ * apenas adaptou-se o codigo para chegar na solucao correta!
  */
 
 #include <stdio.h>
@@ -19,37 +21,39 @@ const char senha[] = "SENHASECRETA";
 char decrypter(char c, char k) 
 {
 	/* onde C = c0,c1.. (caracteres); K = k0, k1... (chaves); m é a mensagem */
-  char c0, k0, m;	
-  if (c >= 'A' && c <= 'Z') {
-    c0 = c - 'A';
-    k0 = k - 'A';
+  	char c0, k0, m;	
+	if (c >= 'A' && c <= 'Z') 
+  	{
+   		c0 = c - 'A';
+    	k0 = k - 'A';
     
-    /* algoritmo para decifrar codigo segundo pagina da Wikipedia */
-    m = 'A' + (c0 - k0 + ('Z' - 'A' + 1)) % ('Z' - 'A' + 1);
-    return m;
-  } else return c; // caso nao seja uma letra
+    	/* algoritmo para decifrar codigo segundo pagina da Wikipedia */
+    	m = 'A' + (c0 - k0 + ('Z' - 'A' + 1)) % ('Z' - 'A' + 1);
+    	return m;
+  	} 	else return c; // caso nao seja uma letra
 }
 
 int main() {
-  int j=0;
-  int i=0;
+	int j = 0;
+  	int i = 0;
 
-  /* Leitura da entrada */
-  fgets(buffer_entrada, TAM_BUFFER, stdin);
-
-
-  for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++)
-  {
-
-    buffer_saida[i] = decrypter(buffer_entrada[i], senha[j]);
-    j++;
-    if (senha[j]=='\0') j=0;
-  }
-  buffer_saida[i]='\0';
+  	/* Leitura da entrada */
+  	fgets(buffer_entrada, TAM_BUFFER, stdin);
 
 
-  /* Escrever saida na tela */
-  printf("%s\n", buffer_saida);
+	for (i = 0; (i < TAM_BUFFER) && (buffer_entrada[i] != '\0') && (buffer_entrada[i] != '\n'); i++)
+  	{
 
-  return 0;
+    	buffer_saida[i] = decrypter(buffer_entrada[i], senha[j]);
+    	j++;
+    	if (senha[j] == '\0') j = 0;
+  	}
+  	
+  	buffer_saida[i] = '\0';
+
+
+  	/* Escrever saida na tela */
+  	printf("%s\n", buffer_saida);
+
+  	return 0;
 }
